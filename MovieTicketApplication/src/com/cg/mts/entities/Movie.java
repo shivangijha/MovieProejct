e0b0938ebcb8e6289516f6a7450c15a85049f96b
Shivangi Jha <shivangijha1299@gmail.com>
2020-10-24 18:54:54 +0530

    Changes in movies getter and setters added

--- MovieTicketApplication/src/com/cg/mts/entities/Movie.java
+++ MovieTicketApplication/src/com/cg/mts/entities/Movie.java
@@ -8,6 +8,42 @@ public class Movie {
 	private String movieHours;
 	private String movieLanguage;
 	private String movieDescription;
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getMovieGenre() {
+		return movieGenre;
+	}
+	public void setMovieGenre(String movieGenre) {
+		this.movieGenre = movieGenre;
+	}
+	public String getMovieHours() {
+		return movieHours;
+	}
+	public void setMovieHours(String movieHours) {
+		this.movieHours = movieHours;
+	}
+	public String getMovieLanguage() {
+		return movieLanguage;
+	}
+	public void setMovieLanguage(String movieLanguage) {
+		this.movieLanguage = movieLanguage;
+	}
+	public String getMovieDescription() {
+		return movieDescription;
+	}
+	public void setMovieDescription(String movieDescription) {
+		this.movieDescription = movieDescription;
+	}
 	
 	
 }
